2971fdf1aaf7eebe593fe35a8c9c73023b1ae6a8
Andrii Sushko <andrii_sushko@outlook.com>
2018-04-24 09:49:00 +0300

    Unit tests for FestivalController

--- WebCoreLab/Domain/Festival.cs
+++ WebCoreLab/Domain/Festival.cs
@@ -16,5 +16,27 @@ namespace WebCoreLab.Domain
         public virtual string Description { get; set; }
         public virtual string ImageLink { get; set; }
         public ICollection<LineUp> LineUps { get; set; }
+
+        public Festival() { }
+
+        public Festival(/*long _id,*/ string _Name, string _Country, string _City, DateTime _Start, DateTime _End,
+            string _Descr, string _ImagLink)
+        {
+            //Id = _id;
+            Name = _Name;
+            Country = _Country;
+            City = _City;
+            StartDate = _Start;
+            EndDate = _End;
+            Description = _Descr;
+            ImageLink = _ImagLink;
+        }
+
+        public override bool Equals(object obj)
+        {
+            var other = obj as Festival;
+            return this.City == other.City && this.Name== other.Name && this.Country == other.Country 
+                && this.StartDate == other.StartDate && this.EndDate == other.EndDate;
+        }
     }
 }

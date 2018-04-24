02ee70d996fca39eaf5efb6e5f52a166949ab548
ASUSYK\Sofia <sofia.gorlata@gmail.com>
2018-04-24 08:40:52 +0300

    Details for Fest/Artist

---
+++ WebCoreLab/Domain/Subscribtion.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebCoreLab.Domain
+{
+    public class Subscribtion
+    {
+        public int SubsribtionID { get; set; }
+        public long UserID { get; set; }
+        public int ArtistID { get; set; }
+
+        public Artist Artist { get; set; }
+    }
+}
---
+++ WebCoreLab/Migrations/20180423172425_Eloud.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace WebCoreLab.Migrations
+{
+    public partial class Eloud : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

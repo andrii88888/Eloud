ba49790b4bf15a02d5cab3d969f68609e4718355
Andrii Sushko <andrii_sushko@outlook.com>
2018-05-22 09:55:24 +0300

    final tests

---
+++ FestivalsXUnitTest/StatsControllerTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xunit;
+using WebCoreLab.Controllers;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using WebCoreLab.Domain;
+using System.Collections.Generic;
+using WebCoreLab.Data;
+using WebCoreLab.Models;
+
+namespace FestivalsXUnitTest
+{
+    public class StatsControllerTest
+    {
+        [Fact]
+        public void testIndex()
+        {
+            var controller = new StatsController();
+            Assert.NotNull(controller.stats());
+        }
+    }
+}
